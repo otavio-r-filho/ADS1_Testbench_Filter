31e01067f08e379deec6b21a90cbe43ff82011a0
Otávio Gonçalvez Vicente Ribeiro Filho <otavio.r.filho@gmail.com>
2015-01-08 18:05:54 +0100

    Update testbench.vhd

--- simulation/testbench.vhd
+++ simulation/testbench.vhd
@@ -24,14 +24,25 @@ ARCHITECTURE behavior OF testbench IS
  		);
     END COMPONENT;
     
+    COMPONENT Converter
+	 Port ( 
+	      clk : IN std_ulogic;
+			rst : IN std_ulogic;
+			opr	: IN std_ulogic;
+			enable : IN std_ulogic;
+			to_convert : IN std_logic_vector(31 downto 0);
+			valid_result : INOUT std_ulogic;
+			result : OUT std_logic_vector(31 downto 0)
+ 		);
+    END COMPONENT;
+    
 
    --Inputs   
-   signal CLK			: std_logic;
+	 signal CLK			: std_logic;
 	 signal enable		: std_logic;
 	 signal edge		: std_logic;
 	 signal operand		: std_logic_vector(31 downto 0);
-	
-	
+	 	
  	--Outputs
 	signal result		: std_logic_vector(31 downto 0);
 	signal valid		: std_logic;
@@ -39,13 +50,21 @@ ARCHITECTURE behavior OF testbench IS
 	--Dummy signals
 	signal int : integer;
 	signal flt : float32;
+	signal rst : std_logic;
+	signal opr : std_logic;
+	signal to_convert : std_logic_vector(31 downto 0);
+	signal valid_result : std_logic;
+	signal t_result : std_logic_vector(31 downto 0);
+	signal t_enable : std_ulogic;
+	
+		
 
    -- Clock period definitions
    constant clk_period : time := 10 ns;
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: Dummy_Module 
+    uut: Dummy_Module 
 		PORT MAP (
         CLK => CLK,
 		    enable	=> enable,
@@ -54,7 +73,19 @@ BEGIN
 		    result => result,
 		    valid	=> valid
 	     );
-
+   -- Instantiate converter unit
+    cvt: converter
+	   PORT MAP (
+	   CLK => clk,
+	   rst => rst,	   
+	   opr => opr,
+	   t_enable => enable,
+	   to_convert => to_convert,
+	   valid_result => valid_result,
+	   t_result => result);
+	   
+	  
+	   
    -- Clock process definitions
    clk_process :process
    begin
